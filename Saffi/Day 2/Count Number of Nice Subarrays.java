9abd138b681a0d3ce23c0d6814a37c8c10443753
Saffi <sksaffiulla786@gmail.com>
2024-05-27 22:19:30 +0530

    Day 3

--- Saffi/Day 2/Count Number of Nice Subarrays.java
+++ Saffi/Day 2/Count Number of Nice Subarrays.java
@@ -1,15 +1,37 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        // Replace odd number with a 1 and even with a 0
-        // Now we have to just find the count of subarrays with sum k
-        // The problem now becomes similar to that of the binary subarrays problem
+    public int numSubarraysWithSum(int[] nums, int goal) {
 
-        for(int i = 0; i < nums.length; i++) {
-            if((nums[i] & 1) == 0) nums[i] = 0;
-            else nums[i] = 1;
-        }
+        // First way - using HashMap
+        // TC = O(N), SC = O(N)
 
-        return task(nums, k) - task(nums, k - 1);
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        // int prefixSum = 0;
+
+        // map.put(0, 1);
+
+        // int ans = 0;
+
+        // for(int i = 0; i < nums.length; i++) {
+        //     prefixSum += nums[i];
+
+        //     if(map.containsKey(prefixSum - goal)) ans += map.get(prefixSum - goal);
+
+        //     map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        // }
+
+        // return ans;
+
+        // -------------------------------------------------------------
+
+        // 2ND WAY
+        // TC = O(N), SC = O(1)
+
+        // We will find the count of all subarrays with sum <= goal
+        // We will find the count of all subarrays with sum <= goal - 1
+        // We will subtrac tem to get our answer.
+
+        return task(nums, goal) - task(nums, goal - 1);
     }
 
     static int task(int[] nums, int goal) {
