1842981e5e4b4a89ce6def4b0109502ae0596d3d
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-05-29 11:16:27 +0530

    Files updated

--- Siddhant/Day 5/Minimum Bit Flips to Convert Number.java
+++ Siddhant/Day 5/Minimum Bit Flips to Convert Number.java
@@ -36,3 +36,35 @@ class Solution {
 
 -------------------------------------------------------------------------------------------
 Approach - 2
+
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int count=0;
+        while(start!=0 && goal !=0)
+        {
+            if(((start&1)==1 && (goal&1)==0) || ((start&1)==0 && (goal&1)==1))
+            {
+                count++;
+            }
+            start>>=1;
+            goal>>=1;
+        }
+        while(start!=0)
+        {
+            if((start&1)==1)
+            {
+                count++;
+            }
+            start>>=1;
+        }
+        while(goal!=0)
+        {
+            if((goal&1)==1)
+            {
+                count++;
+            }
+            goal>>=1;
+        }
+        return count;
+    }
+}
