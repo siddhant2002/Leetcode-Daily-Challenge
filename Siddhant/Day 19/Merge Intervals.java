c148bfc96f7fd18df2372fff7b267710f67f62c4
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-06-12 00:55:11 +0530

    Files updated

---
+++ Siddhant/Day 19/Merge Intervals.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> ans=new ArrayList<>();
+        Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
+        int start=intervals[0][0],end=intervals[0][1];
+        for(int i=1;i<intervals.length;i++)
+        {
+            if(intervals[i][0]<=end)
+            {
+                end=Math.max(end,intervals[i][1]);
+            }
+            else
+            {
+                ans.add(new int[]{start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
+            }
+        }
+        ans.add(new int[]{start,end});
+        return ans.toArray(new int[0][]);
+    }
+}
