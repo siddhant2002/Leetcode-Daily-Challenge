969677abac35be653bf417fdc0bdcd7a6334c137
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-07-14 14:00:47 +0530

    Files Updated

--- Siddhant/Day 13/Maximum Depth of Binary Tree.java
+++ Siddhant/Day 13/Maximum Depth of Binary Tree.java
@@ -1,9 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int maxDepth(TreeNode root) {
+    public boolean isBalanced(TreeNode root) {
+        boolean flag[]=new boolean[1];
+        flag[0]=true;
+        int val=task(root,flag);
+        return flag[0];
+    }
+    private int task(TreeNode root, boolean flag[])
+    {
         if(root==null)
         {
             return 0;
         }
-        return 1+Math.max(maxDepth(root.left), maxDepth(root.right));
+        int left=1+task(root.left,flag);
+        int right=1+task(root.right,flag);
+        if(Math.abs(left-right)>1)
+        {
+            flag[0]=false;
+        }
+        return Math.max(left,right);
     }
 }
