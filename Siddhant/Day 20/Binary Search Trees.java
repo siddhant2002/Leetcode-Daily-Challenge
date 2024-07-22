11da7f673d4f441f3a5c86ff500b55e1012e4637
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-07-22 23:49:36 +0530

    Files Updated

---
+++ Siddhant/Day 20/Binary Search Trees.java
@@ -0,0 +1,12 @@
+class Solution {
+    static boolean isBSTTraversal(int arr[]) {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i] >= arr[i+1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
