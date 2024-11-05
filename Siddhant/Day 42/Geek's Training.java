91b9b432d794c3a38412b9fe4388056a980c73a4
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-11-05 11:24:50 +0530

    Files Updated

--- Siddhant/Day 42/Geek's Training.java
+++ Siddhant/Day 42/Geek's Training.java
@@ -1,28 +1,53 @@
-class Solution{
-    public int maximumPoints(int points[][],int N){
-        int dp[][]=new int[N+1][3];
-        for(int i=0;i<3;i++)
+class Solution {
+    public int maximumPoints(int arr[][], int n) {
+        int dp[][] = new int[n][4];
+        dp[0][0] = Math.max(arr[0][1], arr[0][2]);
+        dp[0][1] = Math.max(arr[0][0], arr[0][2]);
+        dp[0][2] = Math.max(arr[0][0], arr[0][1]);
+        dp[0][3] = Math.max(arr[0][1], Math.max(arr[0][0], arr[0][2]));
+        for(int ind = 1; ind < n; ind++)
         {
-            dp[N-1][i]=points[N-1][i];
-        }
-        for(int ind=N-1;ind>=0;ind--)
-        {
-            for(int prev_ind=0;prev_ind<3;prev_ind++)
+            int maxi = 0;
+            for(int prev_ind = 0; prev_ind <= 3; prev_ind++)
             {
-                if(prev_ind==0)
+                for(int i=0; i<3; i++)
                 {
-                    dp[ind][prev_ind] = Math.max(points[ind][1]+dp[ind+1][1], points[ind][2]+dp[ind+1][2]);
-                }
-                else if(prev_ind==1)
-                {
-                    dp[ind][prev_ind] = Math.max(points[ind][0]+dp[ind+1][0], points[ind][2]+dp[ind+1][2]);
-                }
-                else
-                {
-                    dp[ind][prev_ind] = Math.max(points[ind][1]+dp[ind+1][1], points[ind][0]+dp[ind+1][0]);
+                    if(prev_ind != i)
+                    {
+                        dp[ind][prev_ind] = Math.max(dp[ind][prev_ind], arr[ind][i] + dp[ind-1][i]);
+                    }
                 }
             }
+            
         }
-        return Math.max(dp[0][0], Math.max(dp[0][1],dp[0][2]));
+        return dp[n-1][3];
+    }
+}
+
+---------------------------------------------------------------------------------------------------------
+
+class Solution {
+    public int maximumPoints(int arr[][], int n) {
+        int prev[] = new int[4];
+        prev[0] = Math.max(arr[0][1], arr[0][2]);
+        prev[1] = Math.max(arr[0][0], arr[0][2]);
+        prev[2] = Math.max(arr[0][0], arr[0][1]);
+        prev[3] = Math.max(arr[0][1], Math.max(arr[0][0], arr[0][2]));
+        for(int ind = 1; ind < n; ind++)
+        {
+            int cur[] = new int[4];
+            for(int prev_ind = 0; prev_ind <= 3; prev_ind++)
+            {
+                for(int i=0; i<3; i++)
+                {
+                    if(prev_ind != i)
+                    {
+                        cur[prev_ind] = Math.max(cur[prev_ind], arr[ind][i] + prev[i]);
+                    }
+                }
+            }
+            prev = cur;
+        }
+        return prev[3];
     }
 }
